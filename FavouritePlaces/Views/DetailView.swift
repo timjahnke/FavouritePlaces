90f62a3037ab2295f386eebc39165f6a946fbcaf
Timothy Jahnke <tim.jahnke@hotmail.com>
2022-05-12 15:39:20 +1000

    Added bold to detail edit headings. Added commenting on DetailView.

--- FavouritePlaces/Views/DetailView.swift
+++ FavouritePlaces/Views/DetailView.swift
@@ -19,23 +19,25 @@ struct DetailView: View {
         // If Edit Mode is active, create a list and render plain text with Class Place data
         if(editMode?.wrappedValue == .inactive) {
             List {
+                // Create section with place title
                 Section(header:
                     Text(place.placeTitle)
                     .foregroundColor(.black)
                     .font(.system(size: 30))
                     .fontWeight(.bold)) {}
-                // If place url starts with https:// and ends with .jpg or .png render Detail Image View. Has larger image size.
+                // If place url starts with https:// and ends with .jpg or .png
                 if(
                     (place.placeUrl.hasPrefix("https://") && place.placeUrl.hasSuffix(".jpg")) ||
                     (place.placeUrl.hasPrefix("https://") && place.placeUrl.hasSuffix(".png"))
                     )
                     {
+                        // render Detail Image View. Has larger image size.
                         Detail_ImageView(place: place)
                     // Else render system image default. Default string is "".
                     } else {
                         Image(systemName: "location.square").foregroundColor(.green)
                     }
-                    // Check if details is empty. Will display default text if empty.
+                    // Check if details is an empty string. Will display default text if empty.
                     HStack {
                         if(place.placeDetails.count > 0) {
                             Text(place.placeDetails)
@@ -43,12 +45,14 @@ struct DetailView: View {
                             Text("Details: ")
                         }
                     }
+                // Display latitude and longitude together vertically in list row
                     VStack {
+                        // Create a row displaying inline heading and latitude.
                         HStack{
                             Text("Latitude:")
                             Text(place.placeLatitude)
                         }
-                        
+                        // Create a row displaying inline heading and longitude.
                         HStack{
                             Text("Longitude:")
                             Text(place.placeLongitude)
@@ -70,7 +74,7 @@ struct DetailView: View {
                 // Create a horizontal layout to display title and title textfield.
                 // On commit save to context else throw error.
                 HStack {
-                    Text("Title: ")
+                    Text("Title: ").bold()
                     TextField("Edit title", text: $place.placeTitle, onCommit: {
                         // Attempt save else throw fatalError
                         do { try viewContext.save()}
@@ -81,7 +85,7 @@ struct DetailView: View {
                 // Create a vertical layout to display text inline with textfield.
                 // On commit save to context else throw error.
                 VStack {
-                    Text("Image URL")
+                    Text("Image URL").bold()
                     TextField("Edit image URL", text: $place.placeUrl, onCommit: {
                         // Attempt save else throw fatalError
                         do { try viewContext.save()}
@@ -93,7 +97,7 @@ struct DetailView: View {
                 // On commit save to context else throw error.
                 VStack{
                     padding(2)
-                    Text("Enter Place Detail Below:")
+                    Text("Enter Place Detail Below:").bold()
                     TextField("Edit place details", text: $place.placeDetails, onCommit: {
                         // Attempt save else throw fatalError
                         do { try viewContext.save()}
@@ -103,7 +107,7 @@ struct DetailView: View {
                 // Create a horizontal layout to display text inline with textfield.
                 // On commit save to context else throw error.
                 HStack{
-                    Text("Latitude: ")
+                    Text("Latitude: ").bold()
                     TextField("Edit place latitude", text: $place.placeLatitude, onCommit: {
                         // Attempt save else throw fatalError
                         do { try viewContext.save()}
@@ -114,14 +118,13 @@ struct DetailView: View {
                 // Create a horizontal layout to display text inline with textfield.
                 // On commit save to context else throw error.
                 HStack {
-                    Text("Longitude: ")
+                    Text("Longitude: ").bold()
                     TextField("Edit place longitude", text: $place.placeLongitude, onCommit: {
                         // Attempt save else throw fatalError
                         do { try viewContext.save()}
                         catch{ fatalError()}
                     })
                 }
-                
             }
             // Create a toolbar edit button trailing.
             .toolbar {
--- FavouritePlacesTests/FavouritePlacesTests.swift
+++ FavouritePlacesTests/FavouritePlacesTests.swift
@@ -25,6 +25,47 @@ class FavouritePlacesTests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
+    
+    //  (place.placeUrl.hasPrefix("https://") && place.placeUrl.hasSuffix(".jpg")) ||
+    // (place.placeUrl.hasPrefix("https://") && place.placeUrl.hasSuffix(".png"))
+    
+    //      if(place.placeDetails.count > 0) {
+    // Check item name equals matching string
+//    func testItemEqual() {
+//        let name = "Milk"
+//        let item = ItemModel(name: "Milk", isChecked: true)
+//        XCTAssertEqual(item.name, name)
+//    }
+//
+//    // Check item boolean
+//    func testItemIsChecked() {
+//        let item = ItemModel(name: "Bread", isChecked: true)
+//        XCTAssertTrue(item.isChecked)
+//    }
+//
+//    // Check item name is greater than 2 characters
+//    func testItemNameLength() {
+//        let item = ItemModel(name: "Chicken", isChecked: false)
+//        XCTAssertGreaterThan(item.name.count, 2)
+//    }
+//
+//    // Check item name and boolean is not nil
+//    func testItemNotNil() {
+//        let item = ItemModel(name: "Butter", isChecked: false)
+//        XCTAssertNotNil(item.name)
+//        XCTAssertNotNil(item.isChecked)
+//    }
+
+    
+    // Check item array is not empty
+    func testItemArray() {
+        let items: [ItemModel] = [
+            ItemModel(name: "Milk", isChecked: true),
+            ItemModel(name: "Bread", isChecked: false),
+            ItemModel(name: "Chicken", isChecked: false),
+        ]
+        XCTAssertNotNil(items)
+    }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
