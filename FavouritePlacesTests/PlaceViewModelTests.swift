497794a1b7e0c6161293317c98cb59209b4179d1
Timothy Jahnke <tim.jahnke@hotmail.com>
2022-05-19 20:27:55 +1000

    Milestone 2

--- FavouritePlacesTests/PlaceViewModelTests.swift
+++ FavouritePlacesTests/PlaceViewModelTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import MapKit
 
 class PlaceViewModel: XCTestCase {
 
@@ -18,26 +19,35 @@ class PlaceViewModel: XCTestCase {
 //    }
     
     
-//    func testGetterAndSetters() {
-//        var person = Person(name: "hello")
-//        XCTAssertEqual(person.name, "hello")
-//
-//        person.name = "goodbye"
-//        XCTAssertEqual(person.name, "goodbye")
-//
-//    }
-    
-    // Create a region
-//    var region: MKCoordinateRegion {
-//        get {
-//            // Use Function below for creating computed region using computed coordinates
-//            createCoordinates(centerLatitude: placeLatitude, centerLongitude: placeLongitude)
-//        }
-//        set {
-//            // Store computed values below with coordinates in new value
-//            placeLatitude = newValue.center.latitude
-//            placeLongitude = newValue.center.longitude
-//        }
-//    }
+    func testGetterAndSetters() {
+        // Create a test class to test getters and setters for test assertion.
+        class testPlace {
+            var title: String
+            var details: String
+            
+            init(title: String, details: String) {
+                self.title = title
+                self.details = details
+            }
+        }
+        // Test getter and assert equal boolean
+        let place = testPlace(title: "hello", details: "the details")
+        XCTAssertEqual(place.title, "hello")
+        
+        // Test setter and assert equal boolean
+        place.title = "goodbye"
+        XCTAssertEqual(place.title, "goodbye")
 
+    }
+    
+    // Create regions to compare for test equal assertion
+    func testRegion() {
+        // Create identical regions to compare based on practice in Place ViewModel
+        let region1 = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -27.4705, longitude: 153.0260), latitudinalMeters: 5000, longitudinalMeters: 5000)
+        
+        let region2 = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -27.4705, longitude: 153.0260), latitudinalMeters: 5000, longitudinalMeters: 5000)
+        
+        // Test regions for equality
+        XCTAssertEqual(region1, region2)
+    }
 }
